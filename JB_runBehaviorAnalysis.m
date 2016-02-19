fe7ac71d4b49263ba008e83b5be4a6c7cc74eda5
jenbrown72@gmail.com <jenbrown72@gmail.com>
2016-02-18 16:34:03 -0800

    updated

--- JB_runBehaviorAnalysis.m
+++ JB_runBehaviorAnalysis.m
@@ -6,18 +6,18 @@ currDirectory = cd;
 dirFolders = dir;
 folders = dirFolders(arrayfun(@(x)x.name(1),dirFolders)~='.');
 
-for h = 1:length(folders)
+for h = 1:length(folders) %for each mouse ID folder
     
-    tempMousedataDir = char(strcat(currDirectory,'\',folders(h).name));
-    cd(tempMousedataDir);
+    tempMousedataDir = char(strcat(currDirectory,'\',folders(h).name)); 
+    cd(tempMousedataDir); %go to the specific mouse folder
     
-    [newTxtFileCount] = JB_loadTxtFile;
+    [newTxtFileCount] = JB_loadTxtFile; %run load all txt files, will only upload new files
     
-    if newTxtFileCount>0
+  if newTxtFileCount>0 %if new files are found, run basic BehaviorProperties
     
-    JB_basicBehaviorProperties;
+    JB_basicBehaviorProperties(0); %0 dont plot figures, 1 plot figures
      
-    end
+  end
     
 currDirectory;
 end
